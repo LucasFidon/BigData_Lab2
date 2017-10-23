ede5580d84c01589bf68eb4f4fb34466ad78cbfc
Lucas Fidon <lucas.fidon@student.ecp.fr>
2017-10-23 02:41:42 -0700

    added exercise 4: compute mean and added ncdc data for 2015

---
+++ ComputingMean/src/cs/Lab2/ComputeMean/NCDCData.java
@@ -0,0 +1,19 @@
+package cs.Lab2.ComputeMean;
+
+public class NCDCData {
+	public String month;
+	public float temperature;
+	
+	public NCDCData(String month, float temperature) {
+		super();
+		this.month = month;
+		this.temperature = temperature;
+	}
+
+	public NCDCData() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
